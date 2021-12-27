af155873cdfe4a004747d6ccae79d6bcc7ea99a5
aka_lu <jenny50626000@gmail.com>
2021-12-27 18:48:24 +0800

    Solve useRouter problem (loop)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,29 @@
 import '../styles/globals.css'
 import "antd/dist/antd.css";
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import { useRouter, NextRouter } from 'next/router';
+import { useState, useEffect, useRef,  } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  // TODO: warning: Throttling navigation to prevent the browser from hanging. See <URL>. Command line switch --disable-ipc-flooding-protection can be used to bypass the protection
+  const routerPush = usePush()
   useEffect(() => {
-    router.push('/about');
-  });
-
+    routerPush('/about');
+  }, [routerPush]);
+  
   return <Component {...pageProps} />
+}
+
+// solve: warning: Throttling navigation to prevent the browser from hanging. See <URL>. Command line switch --disable-ipc-flooding-protection can be used to bypass the protection
+function usePush(): NextRouter['push'] {
+  const router = useRouter();
+  const routerRef = useRef(router);
+
+  routerRef.current = router;
+
+  const [{ push }] = useState<Pick<NextRouter, 'push'>>({
+      push: path => routerRef.current.push(path),
+  });
+  return push;
 }
 
 export default MyApp
