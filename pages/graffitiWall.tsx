d7e66ca3eacd1bb1c4e8425c494074a7881b369a
aka_lu <jenny50626000@gmail.com>
2022-01-04 17:46:30 +0800

    Add form on page 'graffitiWall'

--- pages/graffitiWall.tsx
+++ pages/graffitiWall.tsx
@@ -1,40 +1,117 @@
-import { Alert, Col, Layout, Row, Typography } from "antd";
-import { WarningOutlined } from "@ant-design/icons";
+import {
+  Alert,
+  Col,
+  Layout,
+  Row,
+  Typography,
+  Form,
+  Input,
+  Card,
+  Button,
+  message,
+} from "antd";
+import { WarningOutlined, HighlightOutlined } from "@ant-design/icons";
 import { Content } from "antd/lib/layout/layout";
 import { NextPage } from "next";
-import { backgroundColor, mainColor } from "../shared/colors";
 import { menu } from "../components/menu/mainMenu";
 import Marquee from "react-fast-marquee";
+import style from "../styles/graffitiWall.module.css";
 
 const { Title } = Typography;
 
 const GraffitiWall: NextPage = () => {
   const mainMenu = menu;
-  const message = (
-    <Title style={{ color: mainColor }}>
-      <WarningOutlined /> Coming soon ... 
+
+  const msg = (
+    <Title className="myTitle">
+      <WarningOutlined /> Coming soon ...
     </Title>
   );
 
+  const graffitiWall = () => {
+    return (
+      <Card>
+        <Alert
+          banner
+          message={
+            <Marquee pauseOnHover gradient={false} speed={180}>
+              <Row justify="space-around" align="middle" gutter={24}>
+                <Col span={8}>{msg}</Col>
+                <Col span={8}>{msg}</Col>
+                <Col span={8}>{msg}</Col>
+              </Row>
+            </Marquee>
+          }
+        />
+      </Card>
+    );
+  };
+
+  const msgForm = () => {
+    const pasteClick = (values: { userName: string; content: string }) => {
+      message.error(
+        <div >
+          <Row>目前還在施工中 &gt;&lt;</Row>
+          <Row>抱歉了 {values.userName}</Row>
+        </div>
+      );
+    };
+
+    return (
+      <Row justify="center">
+        <Col span={18} lg={14}>
+          <Card className="cardStyles">
+            <Typography>
+              <Title className="title">
+                <HighlightOutlined />
+                塗鴉
+              </Title>
+              <Form
+                name="msgForm"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 10 }}
+                onFinish={pasteClick}
+              >
+                <Form.Item
+                  label="我想說"
+                  name="content"
+                  rules={[{ required: true, message: "想說的話要打上來唷><" }]}
+                >
+                  <Input.TextArea></Input.TextArea>
+                </Form.Item>
+                <Form.Item
+                  label="綽號"
+                  name="userName"
+                  rules={[{ required: true, message: "替塗鴉簽個名吧><" }]}
+                >
+                  <Input></Input>
+                </Form.Item>
+                <Form.Item wrapperCol={{ span: 24 }}>
+                  <Button
+                    type="dashed"
+                    htmlType="submit"
+                    className={style.pasteBtn}
+                  >
+                    貼上
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Typography>
+          </Card>
+        </Col>
+      </Row>
+    );
+  };
+
   return (
-    <Layout style={{ backgroundColor: backgroundColor }}>
+    <Layout className="pageLayout">
       {mainMenu()}
-      <Content style={{ paddingBottom: "3rem" }}>
-        <Layout id="name" style={{ paddingTop: "4rem" }}>
-          <Typography>
-            <Alert
-              banner
-              message={
-                <Marquee pauseOnHover gradient={false}>
-                  <Row justify="space-around" align="middle" gutter={24}>
-                    <Col span={8}>{message}</Col>
-                    <Col span={8}>{message}</Col>
-                    <Col span={8}>{message}</Col>
-                  </Row>
-                </Marquee>
-              }
-            />
-          </Typography>
+      <Content className="pageContent">
+        <Layout id="graffitiWall" className="pageLayout subLayout">
+          {graffitiWall()}
+        </Layout>
+        <Layout id="form" className="pageLayout subLayout">
+          {msgForm()}
         </Layout>
       </Content>
     </Layout>
